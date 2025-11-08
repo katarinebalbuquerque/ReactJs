965d84e6366ba911dbd006d9f0abbdf468df7762
Katarine Albuquerque <katarine.bez.albuquerque@gmail.com>
2025-11-08 18:58:54 -0300

    Delete sabor-da-terra directory

--- sabor-da-terra/src/redux/cart/action-types.js
+++
@@ -1,8 +0,0 @@
-const CartActionTypes = {
-    ADD_PRODUCT: "cart/addProduct",
-    REMOVE_PRODUCT: "cart/removeProduct",
-    INCREMENT_QUANTITY: "cart/incrementQuantity",
-    DECREMENT_QUANTITY: "cart/decrementQuantity",
-}
-
-export default CartActionTypes;
--- sabor-da-terra/src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-import UserReducer from "./user/reducer";
-import CartReducer from "./cart/reducer";
-
-const rootReducer = combineReducers({UserReducer, CartReducer});
-
-export default rootReducer;
